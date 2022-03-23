2eaf4770efce186dda276562d8f2b617c53ae6fb
johncalabro <j.calabro4@gmail.com>
2022-03-23 15:53:44 -0400

    added an onclick event to a paragraph

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 //h2.textContent = "This content added by JavaScript";
 
 //document.querySelector("body").appendChild(h2);
+
+function addingEventListener() {
+    const John = document.getElementById('John');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    John.addEventListener('click', clickAlert);
+}
